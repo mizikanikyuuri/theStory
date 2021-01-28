f8bf80681a1e373e0ac1090618b245714a1f38bc
mizikanikyuuri <baten46-x@yahoo.co.jp>
2021-01-28 12:43:17 +0900

    adding jenkins settings

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ SETTING_FILE_DIR=$1
 
 sudo cp ./app /usr/src/app
 sudo python3 ./app/protostory/manage.py collectstatic --noinput
-sudo sh updateSettings.sh $SETTING_FILE_DIR $WORK_DIR
+sudo sh $SETTING_FILE_DIR/updateSettings.sh $SETTING_FILE_DIR $WORK_DIR
 /usr/local/bin/supervisorctl restart all
