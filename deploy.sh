c54dcb2a9d1ef909b5948d23220a7e47f74a6c80
mizikanikyuuri <baten46-x@yahoo.co.jp>
2021-01-29 17:58:16 +0900

    modded jenkins files

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ APP_ADMIN_USER=$2
 
 sudo cp -r ./app /usr/src
 sudo python3 ./app/protostory/manage.py collectstatic --noinput
-sudo sh $SETTING_FILE_DIR/updateSettings.sh $SETTING_FILE_DIR $WORK_DIR $PROJECT_NAME
+sudo sh $SETTING_FILE_DIR/updateSettings.sh $SETTING_FILE_DIR $WORK_DIR $PROJECT_NAME $APP_ADMIN_USER
 sudo /usr/local/bin/supervisorctl restart all
