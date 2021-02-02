d371f53209b71192e4ec24dfd09c68daf0e52a37
mizikanikyuuri <baten46-x@yahoo.co.jp>
2021-02-02 15:08:36 +0900

    modified websocket and redirect url relative, made environment values

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ PROJECT_NAME=protostory
 APP_ADMIN_USER=$2
 
 . /etc/profile.d/setProtoStoryValues.sh
-cp -r ./app /usr/src
-python3 ./app/protostory/manage.py collectstatic --noinput
-sh $SETTING_FILE_DIR/updateSettings.sh $SETTING_FILE_DIR $WORK_DIR $PROJECT_NAME $APP_ADMIN_USER
+sudo cp -r ./app /usr/src
+sudo -E bash python3 ./app/protostory/manage.py collectstatic --noinput
+sudo -E bash sh $SETTING_FILE_DIR/updateSettings.sh $SETTING_FILE_DIR $WORK_DIR $PROJECT_NAME $APP_ADMIN_USER
 sudo /usr/local/bin/supervisorctl restart all
